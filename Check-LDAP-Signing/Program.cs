65db3d607ecd07b958d708504f6bf7197b671856
S3cur3Th1sSh1t <S3cur3Th1sSh1t@github.com>
2021-08-01 16:57:16 +0200

    First commit

---
+++ Check-LDAP-Signing/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.DirectoryServices.Protocols;
+
+namespace Check_LDAP_Signing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("[-] You have to pass the FQDN as argument");
+                return;
+            }
+
+            var hostname = args[0];
+
+            bool LDAPSigningEnforced = true;
+            bool LDAPSSigningEnforced = true;
+
+            int Port = 389;
+            string DN = hostname + ":" + Port;
+
+            LdapConnection c = new LdapConnection(DN);
+            c.SessionOptions.Signing = false;
+            c.SessionOptions.SecureSocketLayer = false;
+
+            Console.WriteLine("[*] Connecting to " + DN);
+
+            try
+            {
+                c.Bind();
+            }
+            catch 
+            {
+                Console.WriteLine("[-] LDAP not accessible");
+            }
+
+            if (!(c.SessionOptions.Signing))
+            {
+                LDAPSigningEnforced = false;
+            }
+
+            Port = 636;
+            DN = hostname + ":" + Port;
+
+            LdapConnection d = new LdapConnection(DN);
+            d.SessionOptions.Signing = false;
+            d.SessionOptions.SecureSocketLayer = true;
+
+            Console.WriteLine("[*] Connecting to " + DN);
+
+            try
+            {
+                c.Bind();
+            }
+            catch
+            {
+                Console.WriteLine("[-] LDAPS not accessible");
+            }
+
+            if (!(c.SessionOptions.Signing))
+            {
+                LDAPSSigningEnforced = false;
+            }
+
+            if (LDAPSigningEnforced && !(LDAPSSigningEnforced))
+            {
+                Console.WriteLine("[+] " + DN + " has LDAP configured in the state 'Negotiate signing', relaying to LDAPS is therefore possible!");
+            }
+            else if(!(LDAPSigningEnforced) &&!(LDAPSSigningEnforced))
+            {
+                Console.WriteLine("[+] " + DN + " has LDAP configured in the state 'none', relaying to LDAP and LDAPS is therefore possible!");
+            }
+            else if(LDAPSigningEnforced && LDAPSSigningEnforced)
+            {
+                Console.WriteLine("[-] " + DN + " has LDAP configured in the state 'Require Signing', relaying is not possible!");
+            }
+            else if(!(LDAPSigningEnforced) && LDAPSSigningEnforced)
+            {
+                Console.WriteLine("[+] " + DN + " has LDAP configured in the state 'none' or 'Negotiate signing' without LDAPS in use, relaying to LDAP is possible!");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # LDAP-Signing-Scanner
- A little scanner to check the LDAP Signing state
+A little scanner to check the LDAP Signing state.
+
+The idea is to connect to a DC via LDAP and LDAPS with signing set to false from the client side. The DC can either force signing and ignore the client request or accept it to not use signing.
+
+https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/network-security-ldap-client-signing-requirements
+
+The results from both connections tell us which option is in use:
+
+| LDAP result   | LDAPS result  | Option in use |
+| ------------- | ------------- | ------------- |
+| Signing enforced  | Signing not enforced  | Negotiate signing |
+| Signing not enforced  | Signing not enforced  | none |
+| Signing enforced  | Signing enforced  | Require signing |
+| Signing not enforced  | LDAPS not available  | none or Negotiate signing |
